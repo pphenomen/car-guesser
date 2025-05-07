35bbace54aad437a64ca4ab48e1cb891f2da0991
pphenomen <pphenomen44@gmail.com>
2025-05-07 23:22:03 +0300

    FR2.1: Создание БД и моделей

---
+++ CarGuesser.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CarGuesser.Api.Models;
+
+namespace CarGuesser.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<GameSession> GameSessions => Set<GameSession>();
+        public DbSet<SessionAnswer> SessionAnswers => Set<SessionAnswer>();
+    }
+}
