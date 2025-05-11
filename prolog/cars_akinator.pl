045f1823de892c9b3d0662ebfb9195612d6d5f2c
kozzze <kozlov.ed.vs@mail.ru>
2025-05-12 00:24:24 +0300

    FR1.1. Каркас базы, добавил 50 машин.  Реализовано добавление новой машины, если не угадал изначально
    # Добавить частичное угадывание & еще машин

---
+++ prolog/cars_akinator.pl
@@ -0,0 +1,123 @@
+
+:- dynamic car/9.
+
+car('Toyota Corolla', 2, 1, 1, 2, 3, 2, 1, 0).
+car('BMW 3 Series', 1, 2, 2, 4, 3, 2, 1, 0).
+car('Tesla Model 3', 3, 3, 3, 1, 1, 2, 2, 1).
+car('Ford Mustang', 2, 1, 2, 4, 2, 1, 1, 1).
+car('Audi A4', 1, 2, 2, 3, 4, 2, 2, 0).
+car('Honda Civic', 1, 1, 1, 2, 3, 2, 1, 0).
+car('Chevrolet Camaro', 2, 1, 2, 3, 2, 1, 1, 1).
+car('Nissan Altima', 1, 1, 1, 2, 3, 2, 2, 0).
+car('Mercedes-Benz C-Class', 1, 2, 1, 4, 4, 2, 1, 0).
+car('Subaru Impreza', 3, 1, 1, 2, 3, 3, 2, 0).
+car('Mazda 6', 1, 2, 1, 3, 4, 2, 1, 0).
+car('Ford F-150', 2, 2, 1, 5, 2, 1, 2, 1).
+car('Lexus RX', 1, 3, 1, 4, 4, 3, 1, 0).
+car('Hyundai Elantra', 1, 1, 1, 2, 3, 2, 1, 0).
+car('Volkswagen Golf', 3, 2, 1, 3, 3, 2, 2, 0).
+car('Porsche 911', 2, 2, 2, 5, 4, 2, 1, 1).
+car('Jeep Grand Cherokee', 3, 2, 1, 4, 2, 3, 2, 1).
+car('BMW X5', 1, 2, 1, 4, 4, 3, 1, 0).
+car('Chevrolet Silverado', 2, 2, 1, 5, 2, 1, 2, 1).
+car('Toyota Camry', 1, 2, 1, 3, 3, 2, 1, 0).
+car('Ford Explorer', 3, 2, 1, 4, 2, 3, 2, 0).
+car('Honda Accord', 1, 1, 1, 2, 3, 2, 1, 0).
+car('Hyundai Sonata', 1, 2, 1, 3, 3, 2, 1, 0).
+car('Audi Q7', 1, 3, 1, 4, 4, 3, 1, 0).
+car('Tesla Model X', 3, 3, 3, 1, 1, 3, 2, 1).
+car('Kia Optima', 1, 1, 1, 2, 3, 2, 1, 0).
+car('BMW 5 Series', 1, 2, 1, 4, 4, 2, 1, 0).
+car('Volkswagen Passat', 1, 2, 1, 3, 3, 2, 2, 0).
+car('Chrysler Pacifica', 3, 2, 1, 3, 2, 3, 2, 0).
+car('Chevrolet Tahoe', 2, 2, 1, 5, 2, 3, 1, 1).
+car('Mazda CX-5', 1, 2, 1, 3, 4, 3, 2, 0).
+car('Ford Edge', 1, 2, 1, 3, 2, 3, 2, 0).
+car('Nissan Rogue', 1, 2, 1, 3, 3, 2, 2, 0).
+car('Toyota Highlander', 3, 2, 1, 4, 3, 3, 2, 0).
+car('Porsche Cayenne', 1, 2, 2, 5, 4, 3, 1, 1).
+car('Subaru Outback', 3, 2, 1, 3, 3, 2, 1, 0).
+car('Ford Ranger', 2, 2, 1, 4, 2, 1, 2, 0).
+car('GMC Sierra', 2, 2, 1, 5, 2, 1, 2, 1).
+car('Nissan Murano', 1, 2, 1, 3, 3, 2, 2, 0).
+car('BMW X3', 1, 2, 1, 4, 4, 3, 1, 0).
+car('Lexus GX', 3, 2, 1, 4, 4, 3, 1, 0).
+car('Honda CR-V', 1, 1, 1, 2, 3, 2, 2, 0).
+car('Kia Sorento', 1, 2, 1, 3, 3, 3, 1, 0).
+car('Chevrolet Traverse', 3, 2, 1, 4, 2, 3, 2, 0).
+car('Toyota 4Runner', 3, 2, 1, 4, 3, 3, 2, 1).
+car('Hyundai Tucson', 1, 1, 1, 2, 3, 2, 2, 0).
+car('Volvo XC90', 1, 3, 1, 4, 4, 3, 1, 0).
+car('Jeep Wrangler', 3, 1, 1, 3, 2, 3, 1, 1).
+car('BMW X6', 1, 2, 1, 4, 4, 3, 2, 1).
+car('Ram 1500', 2, 2, 1, 5, 2, 1, 2, 1).
+car('Cadillac Escalade', 2, 2, 1, 5, 4, 3, 1, 1).
+car('Lincoln Navigator', 2, 2, 1, 5, 4, 3, 1, 1).
+car('Audi Q5', 1, 2, 1, 4, 4, 3, 1, 0).
+car('Ford Mustang Mach-E', 3, 3, 3, 1, 2, 2, 1, 1).
+car('Mitsubishi Outlander', 3, 1, 1, 3, 3, 2, 1, 0).
+car('Toyota Tacoma', 2, 2, 1, 4, 3, 1, 2, 0).
+car('Jaguar F-Pace', 1, 2, 1, 4, 4, 3, 1, 1).
+car('Acura MDX', 1, 3, 1, 4, 4, 3, 2, 0).
+car('Infiniti QX80', 2, 2, 1, 5, 4, 3, 1, 1).
+car('Land Rover Range Rover', 1, 2, 2, 5, 4, 3, 1, 1).
+car('Mazda MX-5 Miata', 2, 1, 1, 3, 4, 2, 1, 1).
+car('Mini Cooper', 3, 1, 1, 2, 3, 2, 2, 0).
+car('Mitsubishi Eclipse Cross', 3, 2, 1, 3, 2, 3, 2, 0).
+car('Porsche Macan', 1, 2, 2, 5, 4, 3, 1, 1).
+car('Buick Enclave', 3, 2, 1, 4, 3, 3, 2, 0).
+car('Chevrolet Equinox', 1, 2, 1, 3, 3, 2, 2, 0).
+car('Toyota Prius', 1, 3, 3, 1, 3, 2, 2, 1).
+car('Kia Telluride', 3, 2, 1, 4, 3, 3, 2, 0).
+car('Hyundai Palisade', 1, 2, 1, 3, 3, 3, 2, 0).
+car('Tesla Model S', 3, 3, 3, 1, 1, 2, 2, 1).
+car('Nissan Frontier', 2, 2, 1, 4, 2, 1, 2, 0).
+car('Ford Bronco', 3, 2, 1, 4, 2, 3, 2, 1).
+car('Dodge Durango', 3, 2, 1, 4, 2, 3, 2, 0).
+
+% Задаем вопросы
+ask_question(1, 'Тип кузова?', ['1. Купе', '2. Седан', '3. Хэтчбек']).
+ask_question(2, 'Объем двигателя?', ['1. 1-2 литра', '2. 2-3 литра', '3. 3-4 литра', '4. 4-5 литра']).
+ask_question(3, 'Тип двигателя?', ['1. Бензин', '2. Дизель', '3. Электрический']).
+ask_question(4, 'Мощность?', ['1. 50-100 л.с.', '2. 100-200 л.с.', '3. 200-300 л.с.', '4. 300-400 л.с.', '5. 400-500 л.с.']).
+ask_question(5, 'Страна производства?', ['1. Россия', '2. США', '3. Япония', '4. Германия']).
+ask_question(6, 'Привод?', ['1. Передний', '2. Задний', '3. Полный']).
+ask_question(7, 'Коробка передач?', ['1. Механика', '2. Автомат']).
+ask_question(8, 'Быстрый?', ['1. Да', '2. Нет']).
+
+% Задаем вопрос и получаем ответ
+ask_user(Q, Answer) :-
+    ask_question(Q, Question, Options),
+    format('~w ~w~n', [Question, Options]),
+    read(Answer).
+
+guess_car(Answers) :-
+    car(Car, T, V, D, P, C, G, F, B),
+    Answers = [T, V, D, P, C, G, F, B],
+    format('Угадал: ~w~n', [Car]).
+
+add_car(Name, T, V, D, P, C, G, F, B) :-
+    atom(Name), % Проверяем, что Name — это атом
+    assertz(car(Name, T, V, D, P, C, G, F, B)),
+    format('Автомобиль ~w добавлен в базу данных!~n', [Name]).
+
+start_game :-
+    ask_user(1, Answer1),
+    ask_user(2, Answer2),
+    ask_user(3, Answer3),
+    ask_user(4, Answer4),
+    ask_user(5, Answer5),
+    ask_user(6, Answer6),
+    ask_user(7, Answer7),
+    ask_user(8, Answer8),
+    (   guess_car([Answer1, Answer2, Answer3, Answer4, Answer5, Answer6, Answer7, Answer8])
+    ->  true
+    ;   format('Не удалось угадать! Хотите добавить новый автомобиль? (да/нет)~n'),
+        read(Response),
+        (   Response = 'да'
+        ->  format('Введите марку автомобиля (введите в кавычках, например \'BMW 8 Series\'): '),
+            read(Name),
+            add_car(Name, Answer1, Answer2, Answer3, Answer4, Answer5, Answer6, Answer7, Answer8)
+        ;   format('Спасибо за игру!~n')
+        )
+    ).
