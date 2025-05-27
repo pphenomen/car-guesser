3da5e94e51f212257d940d9e3c20ed24ab97d5bb
temafroloff <temavvs1437@gmail.com>
2025-05-27 22:16:39 +0300

    FR 3.1 create bd connection and base req

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿let connectionString = "Server=192.168.1.22;Database=robot;User=root;Password=allhailklab"
+
+open FSharp.Data.Sql
+
+type sql = SqlDataProvider<ConnectionString = connectionString, DatabaseVendor = Common.DatabaseProviderTypes.POSTGREESQL>
+let ctx = sql.GetDataContext()
+
+
+let getTop10Cars() =
+    query {
+        for session in ctx.``[akinator_cars].[game_sessions]`` do
+        where (session.GuessedCharacter.IsSome)  // Успешные сессии, где угадали автомобиль
+        groupBy session.GuessedCharacter into g
+        sortByDescending (g.Count())
+        select (g.Key, g.Count())
+        take 10
+    } |> Seq.toList
+
+let getSuccessRate() =
+    let total = query { for session in ctx.``[akinator_cars].[game_sessions]`` do count }
+    let correct = query { 
+        for session in ctx.``[akinator_cars].[game_sessions]`` do 
+        where (session.GuessedCharacter.IsSome) 
+        count 
+    }
+    float correct / float total
