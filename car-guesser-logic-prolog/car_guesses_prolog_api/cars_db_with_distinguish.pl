cc3f20f5d9c78a3dd4f3a6c1d53d42785b12696c
DaviKHub <klichenkod2005@gmail.com>
2025-05-29 04:35:22 +0300

    Delete car-guesser-logic-prolog directory

--- car-guesser-logic-prolog/car_guesses_prolog_api/cars_db_with_distinguish.pl
+++
@@ -1,5 +0,0 @@
-distinguish(oka, kika, hopa, ['Да', 'Нет'], 2).
-distinguish(kika, oka, hopa, ['Да', 'Нет'], 1).
-
-distinguish('BMW M5', 'BMW eed5', 'Это спортивная машина?', ['Да', 'Нет'], 2).
-distinguish('BMW eed5', 'BMW M5', 'Это спортивная машина?', ['Да', 'Нет'], 1).
